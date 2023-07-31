b28c5ac27183604923012588dcc465631eccb576
leonardo <leomelo@uolinc.com>
2023-07-31 11:05:59 -0300

    payment-service: Add producer config

--- payment-service/src/main/java/com/leonardo/paymentservice/service/impl/PaymentServiceImpl.java
+++ payment-service/src/main/java/com/leonardo/paymentservice/service/impl/PaymentServiceImpl.java
@@ -2,14 +2,26 @@ package com.leonardo.paymentservice.service.impl;
 
 import com.leonardo.paymentservice.model.Payment;
 import com.leonardo.paymentservice.service.PaymentService;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import java.io.Serializable;
+@RequiredArgsConstructor
 @Log4j2
 @Service
 public class PaymentServiceImpl implements PaymentService {
+
+    private final KafkaTemplate<String, Serializable> kafkaTemplate;
+    @SneakyThrows
     @Override
     public void sendPayment(Payment payment) {
-        log.info("PAYMENT_SERVICE_IMPL ::: Recebi o pagamento {}", payment);
+        log.info("Recebi o pagamento {}", payment);
+        Thread.sleep(1000);
+
+        log.info("Enviando o pagamento...");
+        kafkaTemplate.send("payment-topic", payment);
     }
 }
